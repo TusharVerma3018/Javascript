e0f352ebe79709563537b23bb7f0a382d6fce31d
Tushar Verma <tusharverma3018@gmail.com>
2023-09-21 15:02:05 +0000

    container config done

---
+++ Javascript/variable.js
@@ -0,0 +1,13 @@
+const account_Id=123
+let accountemail="gmail.com"
+var accoutpass="123"
+accountcity="jaipur"
+// account_Id=2 
+accountemail="nw"
+accoutpass="123"
+accountcity="bang"
+console.log(account_Id);
+console.log(accountemail);
+console.log(accoutpass);
+console.log(accountcity);
+console.table([account_Id,accountemail,accoutpass,accountcity])
